44457c46f5147fb65839e02c228157c140ac4980
YAO <yaojiaye2022@gmail.com>
2025-07-11 19:56:47 +0900

    21312321

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://jiayejia.github.io",
-	base: "blog_yao",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
