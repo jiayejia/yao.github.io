519cd39c9076ed8967056376a575c32dad3bf4d4
YAO <yaojiaye2022@gmail.com>
2025-07-11 19:42:14 +0900

    chushihua6

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://yao.github.io",
+	site: "https://jiayejia.github.io/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
